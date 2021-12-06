d21d11218f20a436a5137ba658ee194fd91be0ac
ndrewt <67764955+ndrewt@users.noreply.github.com>
2021-12-06 23:36:52 +0200

    0

---
+++ Node/19 Errors handling/routes/auth.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const {check,body} = require('express-validator/check')
+
+const authController = require('../controllers/auth');
+const User =require('../models/user')
+
+const router = express.Router();
+
+router.get('/login', authController.getLogin);
+
+router.get('/signup', authController.getSignup);
+
+router.post('/login',
+[body('email','Please enter a valid email!')
+  .isEmail()
+  .custom((value,{req})=>{
+      return User.findOne({ email: value })
+        .then(userDoc => {
+          if (!userDoc) {
+          return Promise.reject('Email not exists ,please enter a real one')
+        }
+    })
+    })
+    .normalizeEmail(),
+ body('password','Please enter a password with numbers and text at least 5 characters!')
+ .isLength({min:5})
+ .isAlphanumeric()
+ .trim()
+  
+], authController.postLogin);
+
+router.post('/signup',[check('email')
+.isEmail()
+.withMessage('Please enter a valid email!')
+.custom((value,{req})=>{
+/*if(value === 'and1tarnavskyi@gmail.com'){
+ throw new Error('Email if forbidden')
+}
+return true*/
+  return User.findOne({ email: value })
+    .then(userDoc => {
+      if (userDoc) {
+      return Promise.reject('Email exists, please enter a different one')
+    }
+})
+})
+.normalizeEmail()
+,body('password','Please enter a password with numbers and text at least 5 characters!')
+.isLength({min:5})
+.isAlphanumeric()
+.trim()
+,body('confirmPassword').trim().custom((value,{req})=>{
+    if(value!==req.body.password){
+        throw new Error('Password have to match!')
+    }
+    return true
+})]
+
+, 
+authController.postSignup);
+
+router.post('/logout', authController.postLogout);
+
+router.get('/reset', authController.getReset);
+
+router.post('/reset', authController.postReset);
+
+router.get('/reset/:token', authController.getNewPassword);
+
+router.post('/new-password', authController.postNewPassword);
+
+module.exports = router;
